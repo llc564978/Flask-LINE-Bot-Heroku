01c95525601cee7648f84473de2d2af4bcc5ea60
ZhengRenHong <llc564978@gmail.com>
2021-08-14 15:18:03 +0800

    feature: first commit

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
+# -*- coding: UTF-8 -*-
 import os
 from datetime import datetime
-
 from flask import Flask, abort, request
-
-# https://github.com/line/line-bot-sdk-python
+from datetime import datetime
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
@@ -13,28 +12,42 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/callback", methods=['POST'])
 def callback():
+    signature = request.headers['X-Line-Signature']
 
-    if request.method == "GET":
-        return "Hello Heroku"
-    if request.method == "POST":
-        signature = request.headers["X-Line-Signature"]
-        body = request.get_data(as_text=True)
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    
+    try:
+        print(body, signature)
+        handler.handle(body, signature)
+        
+    except InvalidSignatureError:
+        abort(400)
 
-        try:
-            handler.handle(body, signature)
-        except InvalidSignatureError:
-            abort(400)
-
-        return "OK"
-
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    get_message = event.message.text
+def pretty_echo(event):
 
-    # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
+    sum = 0
+
+    for i in event.message.text.splitlines():
+        a = i.split(" ")
+        d1 = datetime.strptime(a[0], '%H:%M')
+        d2 = datetime.strptime(a[1], '%H:%M')
+        minutes_diff = (d2 - d1).total_seconds() / 60.0
+        minutes_diff = minutes_diff - (minutes_diff % 5)
+        sum += minutes_diff
+    
+    hour = int(sum // 60)
+    minute = int(sum % 60)
+        
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="總共" + str(hour) + "小時" + str(minute) + "分鐘")
+    )
+
+if __name__ == "__main__":
+    app.run()
